fc9872539f216f662ab3b73d69e7ceedaf6d8229
ishikawatatuya <shichuanzi83@gmail.com>
2021-06-05 15:03:10 +0900

    コミット

--- app/src/main/java/com/example/check4/MainActivity.kt
+++ app/src/main/java/com/example/check4/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.check4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var count:Int=0
+
+        button.setOnClickListener{
+            count++
+            counttext.text=count.toString()
+
+        }
     }
 }
