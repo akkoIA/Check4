f63e21963bb78d89656f45f375bb571038dfa5d3
ishikawatatuya <shichuanzi83@gmail.com>
2021-06-05 15:25:56 +0900

    コミット1

--- app/src/main/java/com/example/check4/MainActivity.kt
+++ app/src/main/java/com/example/check4/MainActivity.kt
@@ -12,13 +12,18 @@ class MainActivity : AppCompatActivity() {
 
         var count:Int=0
         var count2:Int=count/4
+        val name1="Life is Teck!"
+        val name2="名古屋土曜"
+        val name3="Androidコース"
+        val name4="あみだ班"
+
 
         button.setOnClickListener{
             count++
             counttext.text=count.toString()
 
             when(count2){
-                0-> print("Life is Teck!")
+                0-> print(count+{$name1})
                 1-> print("名古屋土曜")
                 2-> print("Androidコース")
                 3->print("あみだ班")
